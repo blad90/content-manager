b40d965d10403547915b4e73604421a563492232
bbaez <bbaez_11@hotmail.com>
2018-02-20 14:39:04 -0400

    Added other model classes.

---
+++ ContentManager/Models/Professor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentManager.Models
+{
+    public class Professor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ ContentManager/Models/StudyPlan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentManager.Models
+{
+    public class StudyPlan
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+
+        public University University { get; set; }
+        public int UniversityID { get; set; }
+    }
+}
